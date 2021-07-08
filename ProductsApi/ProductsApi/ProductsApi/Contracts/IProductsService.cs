49e6c3f109765f771387bc9b6c0f059910334042
Michael Diers <md@sreid.de>
2021-07-08 11:59:18 +0200

    depency injection

---
+++ ProductsApi/ProductsApi/ProductsApi/Contracts/IProductsService.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductsApi.Contracts
+{
+	using System.Collections.Generic;
+	using System.Threading.Tasks;
+
+	public interface IProductsService
+	{
+		Task<IEnumerable<IProduct>> ReadProducts();
+	}
+}
---
+++ ProductsApi/ProductsApi/ProductsApi/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+using ProductsApi.Contracts;
+using ProductsApi.Services;
+
+[assembly: FunctionsStartup(typeof(ProductsApi.Startup))]
+
+namespace ProductsApi
+{
+  public class Startup : FunctionsStartup
+  {
+    public override void Configure(IFunctionsHostBuilder builder)
+    {
+      builder.Services.AddSingleton<IProductsService, ProductsService>();
+    }
+  }
+}
