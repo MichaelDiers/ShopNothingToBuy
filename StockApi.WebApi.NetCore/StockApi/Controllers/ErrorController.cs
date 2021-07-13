c6077d8e6470c04985a6e71f80828b47ec0a23b1
Michael Diers <md@sreid.de>
2021-07-13 15:05:10 +0200

    404 route handling

---
+++ StockApi.WebApi.NetCore/StockApi/Controllers/ErrorController.cs
@@ -0,0 +1,27 @@
+﻿namespace StockApi.Controllers
+{
+	using Microsoft.AspNetCore.Mvc;
+	using Newtonsoft.Json.Linq;
+
+	/// <summary>
+	///   Handler for requests of unknown routes.
+	/// </summary>
+	[Route("{*url}", Order = 999)]
+	[ApiController]
+	public class ErrorController : ControllerBase
+	{
+		/// <summary>
+		///   Send a <see cref="NotFoundObjectResult" /> for unknown routes.
+		/// </summary>
+		/// <returns>A <see cref="NotFoundObjectResult" />.</returns>
+		[HttpDelete]
+		[HttpGet]
+		[HttpPost]
+		[HttpPut]
+		public IActionResult HandleError()
+		{
+			var question = new JObject {{"question", "Missed the intersection?"}};
+			return new NotFoundObjectResult(question.ToString());
+		}
+	}
+}
