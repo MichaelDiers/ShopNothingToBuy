a967e19075ba5d7696e0f4b264bc027882b06568
Michael Diers <md@sreid.de>
2021-08-05 10:06:00 +0200

    ci build

---
+++ ShopNothingToBuy.Sdk/ShopNothingToBuy.Sdk.Test/Attributes/IsGuidAttributeTests.cs
@@ -0,0 +1,15 @@
+﻿namespace ShopNothingToBuy.Sdk.Test.Attributes
+{
+	using System.ComponentModel.DataAnnotations;
+	using ShopNothingToBuy.Sdk.Attributes;
+	using Xunit;
+
+	public class IsGuidAttributeTests
+	{
+		[Fact]
+		public void IsValidationAttribute()
+		{
+			Assert.IsAssignableFrom<ValidationAttribute>(new IsGuidAttribute());
+		}
+	}
+}
