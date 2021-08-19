62b3bb0835b491d3fab818a2cd80c4ea769a2bac
Michael Diers <md@sreid.de>
2021-08-19 18:04:34 +0200

    added logging classes

---
+++ ShopNothingToBuy.Sdk/ShopNothingToBuy.Sdk/Contracts/LogLevel.cs
@@ -0,0 +1,33 @@
+﻿namespace ShopNothingToBuy.Sdk.Contracts
+{
+	/// <summary>
+	///   Specifies the log level of a log entry.
+	/// </summary>
+	public enum LogLevel
+	{
+		/// <summary>
+		///   Undefined log level.
+		/// </summary>
+		None = 0,
+
+		/// <summary>
+		///   Log level debug.
+		/// </summary>
+		Debug = 1,
+
+		/// <summary>
+		///   Log level info.
+		/// </summary>
+		Info = 2,
+
+		/// <summary>
+		///   Log  level warning.
+		/// </summary>
+		Warning = 3,
+
+		/// <summary>
+		///   Log level error.
+		/// </summary>
+		Error = 4
+	}
+}
---
+++ ShopNothingToBuy.Sdk/ShopNothingToBuy.Sdk/Extensions/LogLevelConverter.cs
@@ -0,0 +1,54 @@
+﻿namespace ShopNothingToBuy.Sdk.Extensions
+{
+	using System;
+	using Newtonsoft.Json;
+	using ShopNothingToBuy.Sdk.Contracts;
+
+	/// <summary>
+	///   Converter for <see cref="LogLevel" />.
+	/// </summary>
+	public class LogLevelConverter : JsonConverter
+	{
+		/// <summary>
+		///   Checks if <paramref name="objectType" /> can be converted.
+		/// </summary>
+		/// <param name="objectType">The type of the object to be converted.</param>
+		/// <returns>True if an object can be converted and false otherwise.</returns>
+		public override bool CanConvert(Type objectType)
+		{
+			return objectType == typeof(string);
+		}
+
+		/// <summary>
+		///   Convert a value to a <see cref="LogLevel" />.
+		/// </summary>
+		/// <param name="reader">A <see cref="JsonReader" />.</param>
+		/// <param name="objectType">The type of the object.</param>
+		/// <param name="existingValue">The existing value.</param>
+		/// <param name="serializer">The serializer.</param>
+		/// <returns>A <see cref="LogLevel" /> as object.</returns>
+		public override object ReadJson(
+			JsonReader reader,
+			Type objectType,
+			object existingValue,
+			JsonSerializer serializer)
+		{
+			var value = reader.Value as string;
+			return Enum.TryParse<LogLevel>(value, true, out var logLevel) && Enum.IsDefined(typeof(LogLevel), logLevel)
+				? logLevel
+				: LogLevel.None;
+		}
+
+		/// <summary>
+		///   Convert a <see cref="LogLevel" /> to json.
+		/// </summary>
+		/// <param name="writer">A <see cref="JsonWriter" />.</param>
+		/// <param name="value">The value to be converted.</param>
+		/// <param name="serializer">The serializer.</param>
+		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+		{
+			var logLevel = (LogLevel) value;
+			writer.WriteValue(Enum.GetName(typeof(LogLevel), logLevel));
+		}
+	}
+}
