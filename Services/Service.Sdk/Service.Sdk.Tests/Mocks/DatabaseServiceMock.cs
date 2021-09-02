cfd21b01eea577aeb3d29505754348f09ecb88a8
Michael Diers <md@sreid.de>
2021-09-02 21:01:45 +0200

    database service base class

---
+++ Services/Service.Sdk/Service.Sdk.Tests/Mocks/DatabaseServiceMock.cs
@@ -0,0 +1,64 @@
+﻿namespace Service.Sdk.Tests.Mocks
+{
+	using System;
+	using System.Linq;
+	using System.Threading.Tasks;
+	using Service.Sdk.Contracts;
+	using Service.Sdk.Services;
+	using Service.Sdk.Tests.Models;
+
+	internal class DatabaseServiceMock : DatabaseService<StringEntry, string>
+	{
+		private readonly bool exists;
+
+		public DatabaseServiceMock(ILogger logger, IEntryValidator<StringEntry, StringEntry, string> validator, bool exists)
+			: base(logger, validator)
+		{
+			this.exists = exists;
+		}
+
+		protected override Task<ClearResult> ClearEntries()
+		{
+			return Task.FromResult(ClearResult.Cleared);
+		}
+
+		protected override Task<IOperationResult<StringEntry, string, CreateResult>> CreateNewEntry(StringEntry entry)
+		{
+			var result = new OperationResult<StringEntry, string, CreateResult>(CreateResult.Created, entry);
+			return Task.FromResult(result as IOperationResult<StringEntry, string, CreateResult>);
+		}
+
+		protected override Task<IOperationResult<StringEntry, string, DeleteResult>> DeleteExistingEntry(string entryId)
+		{
+			var result = new OperationResult<StringEntry, string, DeleteResult>(
+				DeleteResult.Deleted,
+				new StringEntry(entryId, Guid.NewGuid().ToString()));
+			return Task.FromResult(result as IOperationResult<StringEntry, string, DeleteResult>);
+		}
+
+		protected override Task<ExistsResult> ExistsEntry(string entryId)
+		{
+			return Task.FromResult(this.exists ? ExistsResult.Exists : ExistsResult.NotFound);
+		}
+
+		protected override Task<IOperationListResult<string, ListResult>> ListEntries()
+		{
+			var result = new OperationListResult<string, ListResult>(ListResult.Completed, Enumerable.Empty<string>());
+			return Task.FromResult(result as IOperationListResult<string, ListResult>);
+		}
+
+		protected override Task<IOperationResult<StringEntry, string, ReadResult>> ReadEntry(string entryId)
+		{
+			var result = new OperationResult<StringEntry, string, ReadResult>(
+				ReadResult.Read,
+				new StringEntry(entryId, Guid.NewGuid().ToString()));
+			return Task.FromResult(result as IOperationResult<StringEntry, string, ReadResult>);
+		}
+
+		protected override Task<IOperationResult<StringEntry, string, UpdateResult>> UpdateExistingEntry(StringEntry entry)
+		{
+			var result = new OperationResult<StringEntry, string, UpdateResult>(UpdateResult.Updated, entry);
+			return Task.FromResult(result as IOperationResult<StringEntry, string, UpdateResult>);
+		}
+	}
+}
---
+++ Services/Service.Sdk/Service.Sdk/Services/DatabaseService.cs
@@ -0,0 +1,134 @@
+﻿namespace Service.Sdk.Services
+{
+	using System;
+	using System.Threading.Tasks;
+	using Service.Sdk.Contracts;
+
+	/// <summary>
+	///   Base class for database services.
+	/// </summary>
+	/// <typeparam name="TEntry">The type of the database entries.</typeparam>
+	/// <typeparam name="TEntryId">The type of the database entry id.</typeparam>
+	public abstract class DatabaseService<TEntry, TEntryId> : ServiceBase<TEntry, TEntryId, TEntry, TEntry>,
+		IDatabaseService<TEntry, TEntryId>
+		where TEntry : class, IEntry<TEntryId>
+	{
+		/// <summary>
+		///   Creates a new instance of <see cref="DatabaseService{TEntry,TEntryId}" />.
+		/// </summary>
+		/// <param name="logger">A logger for error messages.</param>
+		/// <param name="validator">A validator for input messages.</param>
+		protected DatabaseService(ILogger logger, IEntryValidator<TEntry, TEntry, TEntryId> validator)
+			: base(logger, validator)
+		{
+		}
+
+		/// <summary>
+		///   Create a new entry. A previous check if the entry already exists if executed.
+		/// </summary>
+		/// <param name="entry">The entry to be created.</param>
+		/// <returns>
+		///   A <see cref="Task" /> whose result is an <see cref="IOperationResult{TEntry,TEntryId,TOperationResult}" />
+		///   that contains the <see cref="CreateResult" />.
+		/// </returns>
+		protected override async Task<IOperationResult<TEntry, TEntryId, CreateResult>> CreateEntry(TEntry entry)
+		{
+			var exists = await this.Exists(entry.Id);
+			switch (exists)
+			{
+				case ExistsResult.Exists:
+					return new OperationResult<TEntry, TEntryId, CreateResult>(CreateResult.AlreadyExists);
+				case ExistsResult.NotFound:
+					return await this.CreateNewEntry(entry);
+				case ExistsResult.InvalidData:
+					return new OperationResult<TEntry, TEntryId, CreateResult>(CreateResult.InvalidData);
+				case ExistsResult.InternalError:
+					return new OperationResult<TEntry, TEntryId, CreateResult>(CreateResult.InternalError);
+				default:
+					throw new ArgumentOutOfRangeException(nameof(exists), exists, "Unhandled value.");
+			}
+		}
+
+		/// <summary>
+		///   Create a new entry without previous existence check.
+		/// </summary>
+		/// <param name="entry">The entry to be created.</param>
+		/// <returns>
+		///   A <see cref="Task" /> whose result is an <see cref="IOperationResult{TEntry,TEntryId,TOperationResult}" />
+		///   that contains the <see cref="CreateResult" />.
+		/// </returns>
+		protected abstract Task<IOperationResult<TEntry, TEntryId, CreateResult>> CreateNewEntry(TEntry entry);
+
+		/// <summary>
+		///   Delete an entry by its id including a previous existence check.
+		/// </summary>
+		/// <param name="entryId">The id of the entry.</param>
+		/// <returns>
+		///   A <see cref="Task" /> whose result is an <see cref="IOperationResult{TEntry,TEntryId,TOperationResult}" />
+		///   that contains the <see cref="DeleteResult" />.
+		/// </returns>
+		protected override async Task<IOperationResult<TEntry, TEntryId, DeleteResult>> DeleteEntry(TEntryId entryId)
+		{
+			var exists = await this.Exists(entryId);
+			switch (exists)
+			{
+				case ExistsResult.Exists:
+					return await this.DeleteExistingEntry(entryId);
+				case ExistsResult.NotFound:
+					return new OperationResult<TEntry, TEntryId, DeleteResult>(DeleteResult.NotFound);
+				case ExistsResult.InvalidData:
+					return new OperationResult<TEntry, TEntryId, DeleteResult>(DeleteResult.InvalidData);
+				case ExistsResult.InternalError:
+					return new OperationResult<TEntry, TEntryId, DeleteResult>(DeleteResult.InternalError);
+				default:
+					throw new ArgumentOutOfRangeException(nameof(exists), exists, "Unhandled value");
+			}
+		}
+
+		/// <summary>
+		///   Delete an entry by its id without a previous existence check.
+		/// </summary>
+		/// <param name="entryId">The id of the entry.</param>
+		/// <returns>
+		///   A <see cref="Task" /> whose result is an <see cref="IOperationResult{TEntry,TEntryId,TOperationResult}" />
+		///   that contains the <see cref="DeleteResult" />.
+		/// </returns>
+		protected abstract Task<IOperationResult<TEntry, TEntryId, DeleteResult>> DeleteExistingEntry(TEntryId entryId);
+
+		/// <summary>
+		///   Update an entry including a previous existence check.
+		/// </summary>
+		/// <param name="entry">The new values of the entry.</param>
+		/// <returns>
+		///   A <see cref="Task" /> whose result is an <see cref="IOperationResult{TEntry,TEntryId,TOperationResult}" />
+		///   that contains the <see cref="UpdateResult" />.
+		/// </returns>
+		protected override async Task<IOperationResult<TEntry, TEntryId, UpdateResult>> UpdateEntry(TEntry entry)
+		{
+			var exists = await this.Exists(entry.Id);
+			switch (exists)
+			{
+				case ExistsResult.Exists:
+					return await this.UpdateExistingEntry(entry);
+				case ExistsResult.NotFound:
+					return new OperationResult<TEntry, TEntryId, UpdateResult>(UpdateResult.NotFound);
+				case ExistsResult.InvalidData:
+					return new OperationResult<TEntry, TEntryId, UpdateResult>(UpdateResult.InvalidData);
+				case ExistsResult.InternalError:
+					return new OperationResult<TEntry, TEntryId, UpdateResult>(UpdateResult.InternalError);
+				default:
+					throw new ArgumentOutOfRangeException(nameof(exists), exists, "Unhandled value");
+			}
+		}
+
+		/// <summary>
+		///   Update an entry without a previous existence check.
+		/// </summary>
+		/// <param name="entry">The new values of the entry.</param>
+		/// <returns>
+		///   A <see cref="Task" /> whose result is an <see cref="IOperationResult{TEntry,TEntryId,TOperationResult}" />
+		///   that contains the <see cref="UpdateResult" />.
+		/// </returns>
+		protected abstract Task<IOperationResult<TEntry, TEntryId, UpdateResult>> UpdateExistingEntry(TEntry entry);
+	}
+}
