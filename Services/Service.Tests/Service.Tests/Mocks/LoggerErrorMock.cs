1f3aa74f4d75e60a35906293cda79578d92d4b67
Michael Diers <md@sreid.de>
2021-09-22 19:22:04 +0200

    Service.Tests nuget

---
+++ Services/Service.Tests/Service.Tests/Mocks/LoggerErrorMock.cs
@@ -0,0 +1,34 @@
+﻿namespace Service.Tests.Mocks
+{
+	using System;
+	using System.Threading.Tasks;
+	using Service.Contracts.Business.Log;
+
+	/// <summary>
+	///   An error logger for messages and <see cref="Exception" />s. The mock does not process the input but throws a
+	///   <see cref="NotImplementedException" />.
+	/// </summary>
+	public class LoggerErrorMock : ILogger
+	{
+		/// <summary>
+		///   No input processing.
+		/// </summary>
+		/// <param name="message">The message to be logged.</param>
+		/// <returns>A <see cref="Task" />.</returns>
+		public Task Error(string message)
+		{
+			throw new NotImplementedException(nameof(LoggerErrorMock));
+		}
+
+		/// <summary>
+		///   No input processing.
+		/// </summary>
+		/// <param name="message">The error message to be logged.</param>
+		/// <param name="ex">The <see cref="Exception" /> to be logged.</param>
+		/// <returns>A <see cref="Task" />.</returns>
+		public Task Error(string message, Exception ex)
+		{
+			throw new NotImplementedException(nameof(LoggerErrorMock));
+		}
+	}
+}
---
+++ Services/Service.Tests/Service.Tests/Mocks/LoggerMock.cs
@@ -0,0 +1,33 @@
+﻿namespace Service.Tests.Mocks
+{
+	using System;
+	using System.Threading.Tasks;
+	using Service.Contracts.Business.Log;
+
+	/// <summary>
+	///   An error logger for messages and <see cref="Exception" />s. The mock does not process the input.
+	/// </summary>
+	public class LoggerMock : ILogger
+	{
+		/// <summary>
+		///   No input processing.
+		/// </summary>
+		/// <param name="message">The message to be logged.</param>
+		/// <returns>A <see cref="Task" />.</returns>
+		public Task Error(string message)
+		{
+			return Task.CompletedTask;
+		}
+
+		/// <summary>
+		///   No input processing.
+		/// </summary>
+		/// <param name="message">The error message to be logged.</param>
+		/// <param name="ex">The <see cref="Exception" /> to be logged.</param>
+		/// <returns>A <see cref="Task" />.</returns>
+		public Task Error(string message, Exception ex)
+		{
+			return Task.CompletedTask;
+		}
+	}
+}
