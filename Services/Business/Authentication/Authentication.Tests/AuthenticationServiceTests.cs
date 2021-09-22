7fac031d73fe644bf823c7ccb1f1b5177460f83b
Michael Diers <md@sreid.de>
2021-09-22 17:51:56 +0200

    auth service

---
+++ Services/Business/Authentication/Authentication.Tests/AuthenticationServiceTests.cs
@@ -0,0 +1,42 @@
+﻿namespace Service.Business.Authentication.Tests
+{
+	using Service.Contracts.Business.Authentication;
+	using Service.Models.Business.Authentication;
+	using Xunit;
+
+	public class AuthenticationServiceTests
+	{
+		[Theory]
+		[InlineData("username", "password", AuthenticationResult.Authorized)]
+		public async void Authenticate(string userName, string password, AuthenticationResult expectedResult)
+		{
+			var service = new AuthenticationService();
+			var actual = await service.Authenticate(
+				new AuthenticationRequest
+				{
+					Password = password,
+					UserName = userName
+				});
+
+			Assert.Equal(expectedResult, actual.Result);
+			if (expectedResult == AuthenticationResult.Authorized)
+			{
+				Assert.NotNull(actual.Response);
+			}
+			else
+			{
+				Assert.Null(actual.Response);
+			}
+		}
+
+		[Fact]
+		public async void Register()
+		{
+			var service = new AuthenticationService();
+			var actual = await service.Register(new RegisterRequest());
+
+			Assert.Equal(RegisterResult.Registered, actual.Result);
+			Assert.NotNull(actual.Response);
+		}
+	}
+}
---
+++ Services/Business/Authentication/Authentication/AuthenticationService.cs
@@ -0,0 +1,39 @@
+﻿namespace Service.Business.Authentication
+{
+	using System;
+	using System.Threading.Tasks;
+	using Service.Contracts.Base;
+	using Service.Contracts.Business.Authentication;
+
+	/// <summary>
+	///   Describes the operations of the authentication service.
+	/// </summary>
+	public class AuthenticationService : IAuthenticationService
+	{
+		/// <summary>
+		///   Authenticates a user and creates a new jwt.
+		/// </summary>
+		/// <param name="request">The request data.</param>
+		/// <returns>
+		///   A Task whose result is an <see cref="IServiceResponse{TOperationResult,TResponse}" /> including the result and
+		///   a jwt if the operation succeeds.
+		/// </returns>
+		public Task<IServiceResponse<AuthenticationResult, string>> Authenticate(IAuthenticationRequest request)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		///   Register a new user.
+		/// </summary>
+		/// <param name="request">The request data.</param>
+		/// <returns>
+		///   A <see cref="Task" /> whose result is an <see cref="IServiceResponse{TOperationResult,TResponse}" /> including
+		///   the <see cref="RegisterResult" /> and a jwt if the operations succeeds.
+		/// </returns>
+		public Task<IServiceResponse<RegisterResult, string>> Register(IRegisterRequest request)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
