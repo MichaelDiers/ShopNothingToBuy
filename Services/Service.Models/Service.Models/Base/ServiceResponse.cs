cab0401c4970d32a5f04cbe6f2c1a84e49428206
Michael Diers <md@sreid.de>
2021-09-22 17:59:50 +0200

    added base models

---
+++ Services/Service.Models/Service.Models/Base/ServiceResponse.cs
@@ -0,0 +1,19 @@
+﻿namespace Service.Models.Base
+{
+	using System;
+	using Service.Contracts.Base;
+
+	/// <summary>
+	///   Describes the result of a service operation and the resulting object.
+	/// </summary>
+	/// <typeparam name="TOperationResult">The type of the result.</typeparam>
+	/// <typeparam name="TResponse">The type of the resulting object.</typeparam>
+	public class ServiceResponse<TOperationResult, TResponse> : ServiceResult<TOperationResult>,
+		IServiceResponse<TOperationResult, TResponse> where TOperationResult : Enum
+	{
+		/// <summary>
+		///   Gets the response of the operation that contains the result object.
+		/// </summary>
+		public TResponse Response { get; set; }
+	}
+}
