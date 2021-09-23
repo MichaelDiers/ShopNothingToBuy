0445e2bbbe6a997f9559fa05a5300f7fdace00d0
Michael Diers <md@sreid.de>
2021-09-23 10:14:07 +0200

    refactor

---
+++ Services/Service.Contracts/Service.Contracts/Client/IEmptyClientResult.cs
@@ -0,0 +1,21 @@
+﻿namespace Service.Contracts.Client
+{
+	using System;
+	using System.Net;
+
+	/// <summary>
+	///   The result of a service call that does not include body data.
+	/// </summary>
+	public interface IEmptyClientResult
+	{
+		/// <summary>
+		///   Gets the <see cref="Exception" /> if occurred at the service call.
+		/// </summary>
+		public Exception Exception { get; }
+
+		/// <summary>
+		///   Gets the resulting status code of the service call.
+		/// </summary>
+		public HttpStatusCode StatusCode { get; }
+	}
+}
