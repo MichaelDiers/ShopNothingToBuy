275f9284b493ecd75d55143688e135de3f3ed149
Michael Diers <md@sreid.de>
2021-10-03 18:57:02 +0200

    user service

--- Services/Crud/User/User/Services/Models/CreateUserEntry.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace User.Services.Models
-{
-	/// <summary>
-	///   Describes the initial user data used for the create operation of the <see cref="UserService" />.
-	/// </summary>
-	public class CreateUserEntry : BaseUser
-	{
-	}
-}
